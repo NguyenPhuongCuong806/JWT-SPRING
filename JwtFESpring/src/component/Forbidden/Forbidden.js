b915878ea209cb814827dcd4c24ed40ab28d4ad4
NguyenPhuongCuong806 <cuongnguyenhd806@gmail.com>
2024-02-29 21:37:55 +0700

    upload

---
+++ JwtFESpring/src/component/Forbidden/Forbidden.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Forbidden.scss'
+import imgforbidden from '../../assest/img/forbidden.jpg'
+
+const Forbidden = () => {
+    return (
+        <div className='forbidden-container'>
+            <div className='img-forbidden'>
+            </div>
+        </div>
+    );
+};
+
+export default Forbidden;
